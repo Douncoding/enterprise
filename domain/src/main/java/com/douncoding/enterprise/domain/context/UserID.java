22ce7212f8b166f3d7cc03be03a17e2e26bdda9a
douncoding <douncoding@gmail.com>
2016-06-23 16:25:33 +0900

    백업#1

--- domain/src/main/java/com/douncoding/enterprise/domain/context/UserID.java
+++ domain/src/main/java/com/douncoding/enterprise/domain/context/UserID.java
@@ -5,6 +5,9 @@ import java.util.UUID;
 /**
  * 접속한 위치의 IP?
  * 접속한 일자?
+ *
+ * UUID 사용 메카니즘
+ * http://android-developers.blogspot.kr/2011/03/identifying-app-installations.html
  */
 public class UserID {
     String UserID;
